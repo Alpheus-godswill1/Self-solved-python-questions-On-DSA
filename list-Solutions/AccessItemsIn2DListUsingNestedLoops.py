6bfdc7c2b89858336893298b91cb71fadf21f7ee
Alpheus Godswill <harryalph9@gmail.com>
2024-02-12 08:23:43 +0100

    Accessing elements in 2D list

--- list-Solutions/AccessItemsIn2DListUsingNestedLoops.py
+++ list-Solutions/AccessItemsIn2DListUsingNestedLoops.py
@@ -3,16 +3,18 @@
 
 # Sample 2D list
 matrix = [
-    [1, 2, 3,23,17],
-    [4, 5, 6,12,23],
-    [7, 8, 9,9,12]
+    [1, 2,3,23,17],
+    [4, 5,6,12,23],
+    [7, 8, 19,9,12,23,67]
 ]
 
 # Access elements using nested loops
 rows = len(matrix)
-cols = len(matrix[0])
-
+cols = len(matrix[1])
+# print(rows)
+# print(cols)
 for i in range(rows):
+    print(i)
     for j in range(cols):
         print(matrix[i][j], end=' ')
     print()
