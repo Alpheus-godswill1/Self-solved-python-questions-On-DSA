e4851a227fbed6551f3e327114065bee8d5af42f
Alpheus Godswill <harryalph9@gmail.com>
2024-02-24 23:42:46 +0100

    learning things properly

--- functionsTests/PrimeNumberCheck.py
+++ functionsTests/PrimeNumberCheck.py
@@ -6,8 +6,8 @@ def is_prime(number):
     for i in range(2, number):
         if number % i == 0:
             return False
-        return True
-print(is_prime(2))
+    return True
+print(is_prime(11))
 for i in range(2, ):
     if is_prime(i):
         print(i, end=' ')
