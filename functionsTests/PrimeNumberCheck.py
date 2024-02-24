e664fa42e0bf49e987a7c04d415162bb835bac4e
Alpheus Godswill <harryalph9@gmail.com>
2024-02-24 12:56:02 +0100

    added the prime number checker script now

--- functionsTests/PrimeNumberCheck.py
+++ functionsTests/PrimeNumberCheck.py
@@ -1,1 +1,13 @@
 # Create a function that checks whether a given number is prime or not.
+def is_prime(number):
+    if number < 2:
+        return False
+    
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+        return True
+print(is_prime(2))
+for i in range(2, ):
+    if is_prime(i):
+        print(i, end=' ')
